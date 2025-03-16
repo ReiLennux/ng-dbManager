7e94c5f330e994ed2b7b5333d3bfec49c57ace7d
ReiLennux <lennyrm343@outlook.com>
2025-03-16 13:49:06 -0600

    initial commit

---
+++ src/app/layout/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'dbManager';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+  
+}
